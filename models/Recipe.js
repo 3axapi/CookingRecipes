d9c3708f0f53c1d8c09577830f325df072fbc682
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-12-29 23:28:06 +0100

    Add mongoose model "Recipe" in Recipe.js

---
+++ models/Recipe.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const RecipeSchema = mongoose.Schema({
+    name: {type: String},
+    ingredients: {type: String},
+    description: {type: String},
+    rating: {type: Number, min: 0, max: 10}
+});
+
+const Recipe = mongoose.model('Recipe', RecipeSchema);
+
+module.exports = Recipe;
