e0cbda5f23431146f3ba1e11f77e6816285355a7
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-12-30 07:07:22 +0100

    Edit changed the order of mongoose connection from route (recipes.js) to app.js

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const mongoose = require('mongoose');
 const app = express();
 const recipesRoutes = require("./routes/recipes");
 
+// mongoose connection
+(async function () {
+    const database = "kitchen";
+    const url = `mongodb://0.0.0.0:27017/${database}`;
+    await mongoose.connect(url);
+    console.log("Connected to MongoDB");
+})();
+
+// middlewares
 app.use(express.json());
 app.use("/api/recipes", recipesRoutes);
 
 
-
+// server maintenance
 const port = process.env.PORT || 8080;
 app.listen(port, () => {console.log(`App is listening on port ${port}`)});
 
@@ -18,7 +27,7 @@ process.on("SIGINT", async () => {
         await mongoose.disconnect();
         console.log("MongoDB connection has disconnected");
     } catch (error) {
-        console.log(error);
+        console.log("Connection error:" + error.message);
     } finally {
         process.exit(0);
     }
--- routes/recipes.js
+++ routes/recipes.js
@@ -3,12 +3,6 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const Recipe = require("../models/Recipe");
 
-const database = "kitchen";
-const url = `mongodb://0.0.0.0:27017/${database}`;
-mongoose.connect(url);
-
-
-
 router.get("/", async (req, res) => {
     try {
         const recipes = await Recipe.find({});
