f19de9cc97e03c79a4ac6b1bb5f42be1bdf9dbc2
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-12-30 07:54:35 +0100

    Improve mongoose functionality in recipes.js: CRUD operations Improve .http queries for CRUD working

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require("mongoose");
 const router = express.Router();
 const Recipe = require("../models/Recipe");
 
@@ -18,7 +17,45 @@ router.get("/:recipeId", async (req, res) => {
         const recipe = await Recipe.find({id: recipeId});
         res.send(recipe);
     } catch (error) {
-        console.log(error);
+        res.status(500).json({error: error});
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        const newRecipe = new Recipe(req.body);
+        const savedRecipe = await newRecipe.save();
+        res.status(201).json(savedRecipe);
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+});
+
+router.delete("/:recipeId", async (req, res) => {
+    try {
+        await Recipe.deleteOne({id: +req.params.recipeId});
+        res.status(200).json({message: 'Recipe deleted'});
+
+    } catch (err) {
+        res.status(500).json({error: err});
+    }
+});
+
+router.put("/:recipeId", async (req, res) => {
+    try {
+        await Recipe.replaceOne({id: +req.params.recipeId}, req.body);
+        res.status(200).json({message: 'Recipe changed successfully'});
+    } catch (err) {
+        res.status(500).json({error: err});
+    }
+});
+
+router.patch("/:recipeId", async (req, res) => {
+    try {
+        await Recipe.updateOne({id: +req.params.recipeId}, {$set: req.body});
+        res.json({message: 'Recipe changed successfully'});
+    } catch (err) {
+        res.status(500).json({error: err});
     }
 })
 
