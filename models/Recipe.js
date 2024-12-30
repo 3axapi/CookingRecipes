98cc654e159d916381ecc4e8955a1f0c66c33e46
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-12-30 10:20:18 +0100

    Improve validation in Module in Recipe.js and in routes in recipes.js

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const RecipeSchema = mongoose.Schema({
-    id: {type: Number},
-    name: {type: String},
-    ingredients: {type: String},
-    description: {type: String},
-    rating: {type: Number, min: 0, max: 10}
+    id: {type: Number, unique: true, required: true},
+    name: {type: String, maxLength: 50, required: true},
+    ingredients: {type: String, required: true},
+    description: {type: String, required: true},
+    rating: {type: Number, min: 0, max: 10, required: true}
 });
 
 const Recipe = mongoose.model('Recipe', RecipeSchema);
--- routes/recipes.js
+++ routes/recipes.js
@@ -4,8 +4,12 @@ const Recipe = require("../models/Recipe");
 
 router.get("/", async (req, res) => {
     try {
-        const recipes = await Recipe.find({});
-        res.send(recipes);
+        const recipes = await Recipe.find({}, {ingredients: true, rating: true});
+
+        if (!recipes.length)
+            res.status(404).json("Recipes not found!");
+        else
+            res.status(200).json(recipes);
     } catch (err) {
         res.status(500).json({error: err});
     }
@@ -14,8 +18,12 @@ router.get("/", async (req, res) => {
 router.get("/:recipeId", async (req, res) => {
     try {
         const recipeId = +req.params.recipeId;
-        const recipe = await Recipe.find({id: recipeId});
-        res.send(recipe);
+        const recipe = await Recipe.find({id: recipeId}, {ingredients: true, rating: true});
+
+        if (!recipe)
+            res.status(404).json({error: 'Recipe not found'});
+        else
+            res.send(recipe);
     } catch (error) {
         res.status(500).json({error: error});
     }
@@ -25,6 +33,7 @@ router.post("/", async (req, res) => {
     try {
         const newRecipe = new Recipe(req.body);
         const savedRecipe = await newRecipe.save();
+
         res.status(201).json(savedRecipe);
     } catch (error) {
         res.status(500).json({error: error});
@@ -33,9 +42,12 @@ router.post("/", async (req, res) => {
 
 router.delete("/:recipeId", async (req, res) => {
     try {
-        await Recipe.deleteOne({id: +req.params.recipeId});
-        res.status(200).json({message: 'Recipe deleted'});
+        const result = await Recipe.deleteOne({id: +req.params.recipeId});
 
+        if (!result.deletedCount)
+            res.status(404).json({error: 'Recipe not found'});
+        else
+            res.status(200).json({message: 'Recipe deleted'});
     } catch (err) {
         res.status(500).json({error: err});
     }
@@ -43,8 +55,14 @@ router.delete("/:recipeId", async (req, res) => {
 
 router.put("/:recipeId", async (req, res) => {
     try {
-        await Recipe.replaceOne({id: +req.params.recipeId}, req.body);
-        res.status(200).json({message: 'Recipe changed successfully'});
+        const result = await Recipe.replaceOne({id: +req.params.recipeId}, req.body);
+
+        if (!result.matchedCount)
+            res.status(404).json({error: 'Recipe not found'});
+        if (!result.modifiedCount)
+            res.status(400).json({error: "Could not update recipe"});
+        else
+            res.status(200).json({message: 'Recipe changed successfully'});
     } catch (err) {
         res.status(500).json({error: err});
     }
@@ -52,8 +70,14 @@ router.put("/:recipeId", async (req, res) => {
 
 router.patch("/:recipeId", async (req, res) => {
     try {
-        await Recipe.updateOne({id: +req.params.recipeId}, {$set: req.body});
-        res.json({message: 'Recipe changed successfully'});
+        const result = await Recipe.updateOne({id: +req.params.recipeId}, {$set: req.body});
+
+        if (!result.matchedCount)
+            res.status(404).json({error: 'Recipe not found'});
+        if (!result.modifiedCount)
+            res.status(400).json({error: 'Could not update recipe'});
+        else
+            res.status(200).json({message: 'Recipe changed successfully'});
     } catch (err) {
         res.status(500).json({error: err});
     }
